d98ebc4b50e034b600a973a0cbc63e48daff05a9
Jhoan Campo <jhoan.campo@lumni.net>
2025-04-09 17:23:42 -0500

    Dynamic routes to applications are implemented

--- src/components/MenuApplicationPage/Application.tsx
+++ src/components/MenuApplicationPage/Application.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Delete from "../Icons/Delete"
 import { IApp } from "./MenuApplicationsPage"
 import ModalDeleteApp from "../Modals/ModalDeleteApp";
+import { Link } from "react-router-dom";
 
 interface IApplicationProps {
   app: IApp,
@@ -21,7 +22,7 @@ const Application = ({ app, deleteApp }: IApplicationProps) => {
   }
 
   return (
-    <div className="flex flex-col justify-center flex-1 min-w-[240px] max-w-[240px] h-48 bg-white rounded-2xl shadow-lg shadow-shadow-primary p-4 relative">
+    <div className="flex flex-col justify-center gap-4 flex-1 min-w-[240px] max-w-[240px] h-48 bg-white rounded-2xl shadow-lg shadow-shadow-primary p-4 relative">
       {
         showModal && (
           <ModalDeleteApp
@@ -37,7 +38,9 @@ const Application = ({ app, deleteApp }: IApplicationProps) => {
       <div className="absolute top-2 right-2 cursor-pointer" onClick={handleDelete}>
         <Delete />
       </div>
-      <h2 className="text-lg font-semibold text-gray-600">{app.title}</h2>
+      <Link to={`/application/${app.title.replace(/\s+/g, '-')}`} className="hover:text-[var(--color-primary)]">
+        <h3>{app.title}</h3>
+      </Link>
       <p className="text-gray-600">{app.description}</p>
     </div>
   )
--- src/components/MenuApplicationPage/MenuApplicationsPage.tsx
+++ src/components/MenuApplicationPage/MenuApplicationsPage.tsx
@@ -3,6 +3,8 @@ import ListApplications from "./ListApplications"
 import MenuApplications from "./MenuApplications"
 import { v4 as uuidv4 } from 'uuid';
 import { useLocalStorage } from "../Hooks/useLocalStorage";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import ApplicationDetail from "./ApplicationDetail";
 
 export interface IApp {
   id: string,
@@ -28,15 +30,29 @@ const MenuApplicationsPage = () => {
   };
 
   return (
-    <div className=" flex justify-center w-full min-h-screen bg-general-fund">
-      <MenuApplications >
-        <ApplicationForm addApp={addApp} />
-        <ListApplications
-          apps={apps}
-          deleteApp={deleteApp}
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className=" flex justify-center w-full min-h-screen bg-general-fund">
+              <MenuApplications >
+                <ApplicationForm addApp={addApp} />
+                <ListApplications
+                  apps={apps}
+                  deleteApp={deleteApp}
+                />
+              </MenuApplications>
+            </div>
+          }
         />
-      </MenuApplications>
-    </div>
+
+        <Route
+          path="/application/:title"
+          element={<ApplicationDetail apps={apps} />}
+        />
+      </Routes>
+    </Router>
   )
 }
 
